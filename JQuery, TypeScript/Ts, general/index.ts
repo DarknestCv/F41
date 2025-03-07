3a33ab83e354ca7a9a848ff4c7b176fdc2c8e8d1
Darknes <riroox@gmail.com>
2025-03-07 17:56:00 +0200

    s

---
+++ JQuery, TypeScript/Ts, general/index.ts
@@ -0,0 +1,29 @@
+// import fs, {readFile} from "node:fs"
+
+// readFile()
+
+
+// tsx
+
+
+// JSDOC
+// .....
+
+
+// type:
+// a
+// b
+
+let version = 10
+
+@param {int} a 
+@param {int} b
+@returns {int}
+
+export const f = (a:number,b: number) => a + b;
+
+
+// ! Integration test - 
+// ! Unit test - 
+// ! UI test - 
+// ! E2E test - тестування додатка в цілому
